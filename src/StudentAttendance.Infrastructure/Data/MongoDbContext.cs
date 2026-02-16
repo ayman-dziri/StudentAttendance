a8e16e69234a59c7b8704398e211739ae1b67023
Mohamed-Idriss Naour <104274703+simonaour@users.noreply.github.com>
2026-02-16 17:28:28 +0000

    commit infrastructure by idriss

---
+++ src/StudentAttendance.Infrastructure/Data/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+
+namespace StudentAttendance.src.StudentAttendance.Infrastructure.Data;
+
+
+
+    public sealed class MongoDbContext
+    {
+    public IMongoDatabase Database { get; }
+
+    public MongoDbContext(MongoDbSettings settings)
+    {
+        var client = new MongoClient(settings.ConnectionString);
+        Database = client.GetDatabase(settings.DatabaseName);
+    }
+
+    public IMongoCollection<T> GetCollection<T>(string name) =>
+        Database.GetCollection<T>(name);
+}
+
