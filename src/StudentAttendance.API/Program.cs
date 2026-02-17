d085ca86f323b59bf7667f3f6fccd582fbad6fdb
unknown <dzirimohamedayman@gmail.com>
2026-02-17 09:16:37 +0000

    feat(domain): implement initial domain model

--- src/StudentAttendance.API/Program.cs
+++ src/StudentAttendance.API/Program.cs
@@ -1,17 +1,6 @@
-using StudentAttendance.src.StudentAttendance.Domain.Entities;
-using StudentAttendance.src.StudentAttendance.Domain.Enums;
-using StudentAttendance.src.StudentAttendance.Infrastructure.Config;
-using StudentAttendance.src.StudentAttendance.Infrastructure.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// Configure Mongo
-builder.Services.Configure<MongoDbSettings>(
-    builder.Configuration.GetSection("MongoSetting"));
-
-builder.Services.AddSingleton<StudentAttendanceDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -31,22 +20,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<StudentAttendanceDbContext>();
-
-    // Petit test d'insertion
-    await context.Users.InsertOneAsync(new User
-    {
-        FirstName = "John",
-        LastName = "Doe",
-        Email = "john.doe@example.com",
-        Password = "hashedpassword",
-        Role = Role.STUDENT,
-        IsActive = true,
-        CreatedAt = DateTime.UtcNow
-    });
-}
-
-
 app.Run();
+
