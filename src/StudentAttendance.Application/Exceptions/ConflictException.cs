2435e96238600bebd997a83183e26a268f27233c
unknown <dzirimohamedayman@gmail.com>
2026-02-20 10:54:05 +0000

    add domain exceptions and integrate exception handling in UserService

---
+++ src/StudentAttendance.Application/Exceptions/ConflictException.cs
@@ -0,0 +1,7 @@
+﻿namespace StudentAttendance.src.StudentAttendance.Application.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message) { }
+    }
+}
--- src/StudentAttendance.Application/Services/UserService.cs
+++ src/StudentAttendance.Application/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using StudentAttendance.src.StudentAttendance.Application.DTOs.user;
+using StudentAttendance.src.StudentAttendance.Application.Exceptions;
 using StudentAttendance.src.StudentAttendance.Application.Interfaces;
 using StudentAttendance.src.StudentAttendance.Application.Mappers;
 using StudentAttendance.src.StudentAttendance.Domain.Entities;
@@ -21,28 +22,48 @@ namespace StudentAttendance.src.StudentAttendance.Application.Services
 
         public async Task CreateUserAsync(CreateUserRequest userDto, CancellationToken ct = default)
         {
-            var user = UserMapper.ToEntity(userDto);
+            if (userDto is null) throw new ValidationException("User data is required"); // si l'objet userDto entré par l'utilisateur est null on lance une exception
+
+            var user = UserMapper.ToEntity(userDto); // on map le dto entré vers l'entité
+            if (string.IsNullOrWhiteSpace(userDto.Password)) throw new ValidationException("Password is required");
 
             var passwordRequest = userDto.Password;
-            user.Password = _passwordHasher.Hash(passwordRequest);
+            user.Password = _passwordHasher.Hash(passwordRequest); // on enregistre le password haché à la place du password entré par l'user
 
-            await _userRepository.AddAsync(user, ct);
+            await _userRepository.AddAsync(user, ct); // enregistrement
         }
 
         public async Task<User?> GetByIdAsync(string id, CancellationToken ct = default)
-            => await _userRepository.GetUserByIdAsync(id, ct);
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ValidationException("User id is required");
+
+            var user = await _userRepository.GetUserByIdAsync(id, ct);
+            if(user is null)    throw new NotFoundException($"User with id : '{id}' not found");
+
+            return user;
+        }
 
         public async Task<List<User>> GetAllUsersAsync(CancellationToken ct = default)
             => await _userRepository.GetUsersAsync(ct);
 
         public async Task<bool> UpdateUserAsync(UpdateUserRequest updateUser, CancellationToken ct = default)
         {
+            if (updateUser is null) throw new ValidationException("Update data is required");
+
             var user = UserMapper.ToEntity(updateUser);
 
-            return await _userRepository.UpdateUserAsync(user, ct);
+            var updated = await _userRepository.UpdateUserAsync(user, ct);
+            if (!updated) throw new Exception("Failed ti update user");
+
+            return updated;
         }
 
         public async Task<bool> DeleteUserAsync(string id, CancellationToken ct = default)
-            => await _userRepository.DeleteUserAsync(id, ct);
+        {
+            var deleted = await _userRepository.DeleteUserAsync(id, ct);
+            if (!deleted)   throw new NotFoundException($"User with id '{id}' not found.");
+
+            return deleted;
+        }
     }
 }
