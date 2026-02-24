52674e8699b026dbb9403e313ecdc1109db02f7c
unknown <dzirimohamedayman@gmail.com>
2026-02-24 10:20:52 +0000

    refactor: move dependency injection configuration to seperate file

--- src/StudentAttendance.Application/Interfaces/IUserService.cs
+++ src/StudentAttendance.Application/Interfaces/IUserService.cs
@@ -10,7 +10,7 @@ namespace StudentAttendance.src.StudentAttendance.Application.Interfaces
         Task<List<UserDetailsResponse>> GetAllUsersAsync(CancellationToken ct = default);
         Task<bool> UpdateUserAsync(string id, UpdateUserRequest updateUser, CancellationToken ct = default);
         Task<bool> DeleteUserAsync(string id, CancellationToken ct = default);
-        Task<User?> GetUserByEmail(string email, CancellationToken ct = default);
+        Task<User?> GetUserByEmailAsync(string email, CancellationToken ct = default);
         Task<List<User>> GetStudentsByGroupAsync(string groupId, CancellationToken ct = default);
     }
 }
