93bce7f06b53c3451095226a7cf54dd442a098da
Kaoutar Belail <kaoutarbelail12@gmail.com>
2026-03-02 09:17:57 +0000

    merge feature/scrum-28-user-login into my-branch (resolve conflicts)

--- src/StudentAttendance.Application/Interfaces/IAuthService.cs
+++ src/StudentAttendance.Application/Interfaces/IAuthService.cs
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 using StudentAttendance.src.StudentAttendance.Application.DTOs.Auth;
-=======
-﻿using StudentAttendance.src.StudentAttendance.Application.DTOs.Auth;
->>>>>>> origin/feature/scrum-28-user-login
 
 namespace StudentAttendance.src.StudentAttendance.Application.Interfaces
 {
     public interface IAuthService
     {
-<<<<<<< HEAD
+        Task<LoginResponseDto> LoginAsync(LoginRequestDto login, CancellationToken cancellationToken = default);
         Task<TokenResponse> RefreshAsync(string refreshToken, CancellationToken ct = default);
         Task LogoutAsync(string userId, CancellationToken ct = default);
-       
-    }
+         }
 }
-=======
-        Task<LoginResponseDto> LoginAsync(LoginRequestDto login, CancellationToken cancellationToken = default);
-    }
-}
->>>>>>> origin/feature/scrum-28-user-login
